bdf9865f2fea949b9c5cfade04b8c51deb7e757e
Ameer-Bazzar1 <bazzarameer3@gmail.com>
2023-06-20 23:33:15 +0200

    edit create for dataBase

---
+++ app/src/main/java/com/example/andriodlab_project1/common/DataBaseHelper.java
@@ -0,0 +1,30 @@
+package com.example.andriodlab_project1.common;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import com.example.andriodlab_project1.admin.AdminDataBaseHelper;
+import com.example.andriodlab_project1.instructor.InstructorDataBaseHelper;
+import com.example.andriodlab_project1.student.StudentDataBaseHelper;
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "my_database";
+    private static final int DATABASE_VERSION = 1;
+    public DataBaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+
+    }
+}
