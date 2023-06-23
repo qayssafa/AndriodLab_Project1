70605fd324ee67c895f1e01a81fb19862f6c2b74
DEEK <deekmuhammad7@gmail.com>
2023-06-23 16:25:31 +0300

    added a multi-select-spinner to choose the prerequesists (not tested yet) now when you clock on the button to insert photo the gallary will be opened (but yet not inserted on the database because i still trying to figure out how to do it) in manifist file i added a permission to open the gallary

--- app/src/main/java/com/example/andriodlab_project1/admin/AdminMainActivity.java
+++ app/src/main/java/com/example/andriodlab_project1/admin/AdminMainActivity.java
@@ -3,11 +3,17 @@ package com.example.andriodlab_project1.admin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.andriodlab_project1.R;
+
+import java.io.File;
+import java.io.FileInputStream;
 
 public class AdminMainActivity extends AppCompatActivity {
     @Override
@@ -23,5 +29,6 @@ public class AdminMainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
 }
