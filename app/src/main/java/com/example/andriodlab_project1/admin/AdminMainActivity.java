d3f72eb02eb63f1f342633f7a4c72ca240c4b040
HP <qayssafa4@gmail.com>
2023-06-30 17:49:39 +0300

    Navigation Drawer first update

--- app/src/main/java/com/example/andriodlab_project1/admin/AdminMainActivity.java
+++ app/src/main/java/com/example/andriodlab_project1/admin/AdminMainActivity.java
@@ -6,13 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.andriodlab_project1.Student2Course.ViewTheStudentsOfAnyCourseActivity;
 import com.example.andriodlab_project1.course.CreateCourseActivity;
 
 import com.example.andriodlab_project1.R;
-import com.example.andriodlab_project1.course.EditOrDeleteAnExistingCourse;
-import com.example.andriodlab_project1.course.EditPage;
-import com.example.andriodlab_project1.course.ViewTheStudentsOfAnyCourse;
-import com.example.andriodlab_project1.course_for_registration.CourseForRegistrationActivity;
+import com.example.andriodlab_project1.course_for_registration.MakeCourseAvailableForRegistrationActivity;
 
 public class AdminMainActivity extends AppCompatActivity {
     private Button CreateCourseButton;
@@ -37,7 +36,7 @@ public class AdminMainActivity extends AppCompatActivity {
         courseForRegestration.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent intent = new Intent(AdminMainActivity.this, CourseForRegistrationActivity.class);
+                Intent intent = new Intent(AdminMainActivity.this, MakeCourseAvailableForRegistrationActivity.class);
                 startActivity(intent);
             }
         });
@@ -45,7 +44,7 @@ public class AdminMainActivity extends AppCompatActivity {
         editOrRemoveCourse.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent intent = new Intent(AdminMainActivity.this, ViewTheStudentsOfAnyCourse.class);
+                Intent intent = new Intent(AdminMainActivity.this, ViewTheStudentsOfAnyCourseActivity.class);
                 startActivity(intent);
             }
         });
