149821a183a7e96c195e975d7395fef440e440a7
DEEK <deekmuhammad7@gmail.com>
2023-07-07 00:08:41 +0300

    courses studied in the center

---
+++ app/src/main/java/com/example/andriodlab_project1/student/CoursesStudiedInTheCenterActivity.java
@@ -0,0 +1,16 @@
+package com.example.andriodlab_project1.student;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.andriodlab_project1.R;
+
+public class CoursesStudiedInTheCenterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_courses_studied_in_the_center);
+    }
+}
